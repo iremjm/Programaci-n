32140d5cdc1cc533150147d34c5e3198aba87980
DW31 <ik012982i9@H012982.NET>
2016-10-03 13:33:34 +0200

    añadido ejercico mes con error de la primera en mayus corregido

--- EjerciciciosGit/src/ejercicios2/EjerMes.java
+++ EjerciciciosGit/src/ejercicios2/EjerMes.java
@@ -16,11 +16,13 @@ public class EjerMes {
 		System.out.println("Ingrese el mes que desea comprobar");
 		//Ingresar mes por teclado 
 		mes =teclado.nextLine();
+		//Convertimos el primer caracter en mayusculas para evitar errores
+		mes=mes.substring(0,1).toUpperCase() + mes.substring(1);
 		
 		//Proceso de datos
 			switch (mes){
 			case "Enero": numMes=1;
-				break;
+			break;
 			case "Febrero": numMes=2;
 			break;
 			case "Marzo": numMes=3;
@@ -43,10 +45,13 @@ public class EjerMes {
 			break;
 			case "Diciembre": numMes=12;
 			break;
-			//default: numMes=error;
+			default: System.out.println("ERROR !!! El mes no existe.");
 			
 			}//switch
-			System.out.println("El mes " + mes + " está en la posición " + numMes);
+			
+			if(numMes<=12 && numMes>=1){
+				System.out.println(mes + " es el " + numMes + "del año");
+			   }//if
 	}
 
 }
