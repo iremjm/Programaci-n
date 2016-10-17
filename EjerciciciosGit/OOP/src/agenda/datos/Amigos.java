ce3b24e669e963d8222860ff44c1b2a43aa79351
DW31 <ik012982i9@H012982.NET>
2016-10-17 12:23:46 +0200

    Añadido agenda

--- EjerciciciosGit/OOP/src/agenda/datos/Amigos.java
+++ EjerciciciosGit/OOP/src/agenda/datos/Amigos.java
@@ -3,11 +3,37 @@ package agenda.datos;
 public class Amigos {
 
 	//Atributos
-	private String nombre;
-	private String email;
-	private String telefono;
-	//Metodos
-	//Amigos
 	
-
-}
+		private String nombre ="";
+		private String email = "";
+		private int telefono =0;
+		
+		//nombre
+		public String getNombre() {
+			return nombre;
+		}
+		public void setNombre(String nombre) {
+			this.nombre = nombre;
+		}
+		//email
+		public String getEmail() {
+			return email;
+		}
+		public void setEmail(String email) {
+			this.email = email;
+		}
+		//telefono
+		public int getTelefono() {
+			return telefono;
+		}
+		public void setTelefono(int telefono) {
+			this.telefono = telefono;
+		}
+		
+		
+		
+	//
+	
+	//
+	
+}//class
--- EjerciciciosGit/OOP/src/agenda/gui/Programa.java
+++ EjerciciciosGit/OOP/src/agenda/gui/Programa.java
@@ -1,7 +1,46 @@
 package agenda.gui;
+import agenda.datos.Amigos;
+import java.util.Scanner;
+
+
 
 public class Programa {
 
 	public static void main(String[] args) {
-	}
-}
+	
+
+		//Inicialimos el _scanner
+		Scanner teclado= new Scanner(System.in);
+		//Creamos el amigo
+		Amigos amigo=new Amigos();
+		String nomAmigo;
+		String emailAmigo;
+		int telefonoAmigo;
+		 //Pedimos los datos por coonsola al usuario
+		System.out.println("Ingrese el nombre en la agenda");
+		 nomAmigo= teclado.nextLine();
+		 //Pedimos el email al usuario
+		 System.out.println("Ingrese el email en la agenda");
+		 emailAmigo= teclado.nextLine();
+		 //Pedimos el teléfono al usuario
+		 System.out.println("Ingrese el teléfono en la agenda");
+		 telefonoAmigo= teclado.nextInt();
+		 
+		 //Asignamos los datos introducidos al personaje
+		 amigo.setNombre(nomAmigo);
+		 amigo.setEmail(emailAmigo);
+		 amigo.setTelefono(telefonoAmigo);
+		 
+		 System.out.println(amigo.getNombre() + " ha sido añaddido a la agenda con el email: " + 
+		 amigo.getEmail() + " y el télefono " + amigo.getTelefono() );
+		 
+		 //Error: no se ha encontrado o cargado la clase principal Agenda.gui.ProgramaAmigos
+		 
+		 
+		
+		
+		
+		
+	}//main
+
+}//class
